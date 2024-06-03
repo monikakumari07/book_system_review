f0d614eb019977051caa05b10249d963c3cc407c
monikakumari07 <84281488+monikakumari07@users.noreply.github.com>
2024-06-03 15:02:05 +0530

    new updates

--- book_review/settings.py
+++ book_review/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
